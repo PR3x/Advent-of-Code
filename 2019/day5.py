d3463bbf9150d7a0210f260155d4822c54657d46
Jacob Rigby <jacobcrigby@gmail.com>
2019-12-10 13:17:25 -0600

    Day 5 Pre bugfixes
    Maybe done, but the "program" needs to be debugged.
    And there may still be bugs in the "computer."

--- 2019/day5.py
+++ 2019/day5.py
@@ -2,47 +2,66 @@
 --- Day 5: Sunny with a Chance of Asteroids ---
 """
 
-import unittest
-from unittest.mock import patch
-
 
 def run(rom):
     # Initially copied from Day 2
     ram = rom.copy()  # Otherwise, rom gets clobbered
-    pc = 0
+    ip = 0
     while True:
-        instruction = str(ram[pc]).rjust(5, "0")
+        instruction = str(ram[ip]).rjust(5, "0")
         mode_parm3 = int(instruction[0])
         mode_parm2 = int(instruction[1])
-        mode_parm3 = int(instruction[2])
+        mode_parm1 = int(instruction[2])
         opcode = int(instruction[3:5])
+        instruction_length = 0
+
+        # Load parameters into "registers"
+        parm1 = 0
+        if mode_parm1 == 0:  # indirect
+            parm1 = int(ram[ram[ip + 1]])
+        elif mode_parm1 == 1:  # direct
+            parm1 = int(ram[ip + 1])
+
+        parm2 = 0
+        if mode_parm2 == 0:  # indirect
+            parm2 = int(ram[ram[ip + 1]])
+        elif mode_parm2 == 1:  # direct
+            parm2 = int(ram[ip + 1])
 
         if opcode == 1:
-            ram[ram[pc + 3]] = ram[ram[pc + 1]] + ram[ram[pc + 2]]
-            pc += 4
+            ram[ram[ip + 3]] = parm1 + parm2  # Writes will *never* be direct
+            instruction_length = 4
         elif opcode == 2:
-            ram[ram[pc + 3]] = ram[ram[pc + 1]] * ram[ram[pc + 2]]
-            pc += 4
+            ram[ram[ip + 3]] = parm1 * parm2
+            instruction_length = 4
         elif opcode == 3:
-            temp = input("> ")
-            ram[ram[pc + 1]] = temp
+            temp = int(input("> "))
+            ram[ram[ip + 1]] = temp
+            instruction_length = 2
         elif opcode == 4:
-            print(ram[ram[pc + 1]])
-            pc += 2
+            output = ram[ram[ip + 1]] if mode_parm3 == 0 else ram[ip + 1]
+            print(output)
+            instruction_length = 2
         elif opcode == 99:
             break
         else:
-            raise Exception("Non-existant opcode", opcode, "Position:", pc)
+            raise Exception("Non-existant opcode", opcode, "Position:", ip)
+
+        ip += instruction_length
 
     return ram
 
 
-class TestRunner(unittest.TestCase):
-    def test1(self):
-        rom = "1101,100,-1,4,0".split(",")
-        out = run(rom)
-        self.assertEqual(out[4], 99)
+def main():
+    data = ""
+    with open("2019/input_day5.txt", "r") as f:
+        data = f.read()
+
+    rom = [int(x) for x in data.split(",")]
+    output = run(rom)
+
+    print(output[0])
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
