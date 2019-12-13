126b45d44268c1db5bd9740347fa9381c302a16f
Jacob Rigby <jacobcrigby@gmail.com>
2019-12-12 21:35:01 -0600

    Day 8 Part 2
    Build the image from its layers.
    Had an issue for a bit when my layers were reversed, but easy fix.

--- 2019/day8.py
+++ 2019/day8.py
@@ -12,27 +12,53 @@ class Layer:
 
     def __eq__(self, other):
         return self.__data == other.__data
-    
+
     def count(self, value):
         return sum(x.count(value) for x in self.__data)
+
+    def __str__(self):
+        out = ""
+        for row in self.__data:
+            for char in row:
+                out += char
+                # I wanted to be fancy, but it didn't work out
+                # if char == "1":
+                #     out += "□"
+                # elif char == "0":
+                #     out += "■"
+                # else:
+                #     out += " "
+            out += "\n"
+        return out
 
 
 class Image:
     def __init__(self, data: str, width: int, height: int):
         self.__layers = []
+        self.__width = width
+        self.__height = height
 
         idx = 0
         while idx < len(data):
             layer = []
-            for _ in range(height):
-                layer.append(data[idx : idx + width])
-                idx += width
+            for _ in range(self.__height):
+                layer.append(data[idx : idx + self.__width])
+                idx += self.__width
             self.__layers.append(Layer(layer))
-    
+
     def part1(self):
         zeroes = [layer.count("0") for layer in self.__layers]
         minlayer = self.__layers[zeroes.index(min(zeroes))]
         return minlayer.count("1") * minlayer.count("2")
+
+    def part2(self):
+        out = (["2"] * self.__width + ["\n"]) * self.__height
+        for layer in self.__layers:
+            layerstr = str(layer)
+            for x in range(len(out)):
+                if out[x] == "2":
+                    out[x] = layerstr[x]
+        return "".join(out)
 
 
 def main():
@@ -41,6 +67,11 @@ def main():
         data = f.readline().strip()
     image = Image(data, 25, 6)
     print(image.part1())
+    print("-----")
+    p2 = image.part2()
+    p2 = p2.replace("0", "□")  # Making it pretty (and readable)
+    p2 = p2.replace("1", "■")
+    print(p2)
 
 
 class Test1(unittest.TestCase):
