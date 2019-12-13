2bc12c667d60636c2384268fae0b8016043c0cf9
Jacob Rigby <jacobcrigby@gmail.com>
2019-12-13 12:18:13 -0600

    Found the bug, but still need to fix it.
    Writes may never be in immediate mode, but they can be in relative mode

--- 2019/day9.py
+++ 2019/day9.py
@@ -2,20 +2,20 @@
 --- Day 9: Sensor Boost ---
 """
 
+import unittest
+
 from typing import List
 import threading  # amps are separate computers
 from queue import SimpleQueue  # Communication between amps
 
-from itertools import permutations  # for all amp settings
-
 
 def load(ram: List[int], pointer: int, mode: int, rb=0) -> int:
     if mode == 0:
-        return int(ram[ram[pointer]])
+        return ram[ram[pointer]]
     elif mode == 1:
-        return int(ram[pointer])
+        return ram[pointer]
     elif mode == 2:
-        return int(ram[ram[pointer + rb]])
+        return ram[ram[pointer] + rb]
 
 
 def run(rom: List[int], inq: SimpleQueue, out: SimpleQueue):
@@ -39,13 +39,13 @@ def run(rom: List[int], inq: SimpleQueue, out: SimpleQueue):
         elif opcode == 1:
             ram[ram[ip + 3]] = load(ram, ip + 1, mode_parm1, rb) + load(
                 ram, ip + 2, mode_parm2, rb
-            )  # Writes will *never* be direct
+            )  # FIXME!
             ip += 4
         # 2: Multiply
         elif opcode == 2:
             ram[ram[ip + 3]] = load(ram, ip + 1, mode_parm1, rb) * load(
                 ram, ip + 2, mode_parm2, rb
-            )
+            )  # FIXME!
             ip += 4
         # 3: Input
         elif opcode == 3:
@@ -54,7 +54,6 @@ def run(rom: List[int], inq: SimpleQueue, out: SimpleQueue):
         # 4: Print
         elif opcode == 4:
             output = load(ram, ip + 1, mode_parm1, rb)
-            # output = ram[ram[ip + 1]] if mode_parm1 == 0 else ram[ip + 1]
             out.put(output)
             ip += 2
         # 5: Jump if True
@@ -83,6 +82,7 @@ def run(rom: List[int], inq: SimpleQueue, out: SimpleQueue):
             else:
                 ram[ram[ip + 3]] = 0
             ip += 4
+        # 9: Set relative
         elif opcode == 9:
             rb += load(ram, ip + 1, mode_parm1, rb)
             ip += 2
@@ -95,9 +95,6 @@ def main():
     with open("2019/input_day9.txt", "r") as f:
         data = f.read()
 
-    # test data
-    data = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
-
     rom = [int(x) for x in data.split(",")] + [0] * 10000
 
     # communication channels
@@ -107,10 +104,51 @@ def main():
     # computer
     boost = threading.Thread(target=run, args=(rom, queue_in, queue_out))
     boost.start()
-    # queue_in.put(1)
+    queue_in.put(1)
     boost.join()
-    print(queue_out.get(False))
+
+    while not queue_out.empty():
+        print(queue_out.get(False))
+
+
+class IntcodeTester(unittest.TestCase):
+    def testQuine(self):
+        data = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+        rom = [int(x) for x in data.split(",")] + [0] * 100
+        queue_in = SimpleQueue()
+        queue_out = SimpleQueue()
+
+        run(rom, queue_in, queue_out)
+
+        output = []
+        while not queue_out.empty():
+            output.append(queue_out.get(False))
+        self.assertEqual(",".join(str(x) for x in output), data)
+
+    def testBignum(self):
+        data = "1102,34915192,34915192,7,4,7,99,0"
+        rom = [int(x) for x in data.split(",")] + [0] * 100
+        queue_in = SimpleQueue()
+        queue_out = SimpleQueue()
+
+        run(rom, queue_in, queue_out)
+
+        output = queue_out.get(False)
+        self.assertEqual(len(str(output)), 16)
+
+    def testMiddlenum(self):
+        data = "104,1125899906842624,99"
+        rom = [int(x) for x in data.split(",")] + [0] * 100
+        queue_in = SimpleQueue()
+        queue_out = SimpleQueue()
+
+        run(rom, queue_in, queue_out)
+
+        output = queue_out.get(False)
+
+        self.assertEqual(str(output), "1125899906842624")
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    unittest.main()
