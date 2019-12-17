38de4fe8e5bba9fdbeaea00f041ac355199469ab
Jacob Rigby <jacobcrigby@gmail.com>
2019-12-17 17:12:36 -0600

    Day 10 Part 1 Try 2
    So far shaping up to be much better code

--- 2019/day10.py
+++ 2019/day10.py
@@ -3,113 +3,59 @@
 """
 
 import unittest
-from typing import Tuple
-from math import gcd
+from typing import Tuple, Optional
+from math import atan2
 
 Coordinate = Tuple[int, int]
 
 
 class AsteroidMap:
-    def __init__(self, data: str):
-        self.__elements = {}
-        self.__processed = {}
+    def __init__(self, data: str = None):
+        if data:
+            self.__processData(data)
+
+    def __processData(self, data: str):
+        self.__asteroids = {}
         split = data.split()
         self.__xdim = len(split[0])  # Assuming rectangular map
         self.__ydim = len(split)
-        self.strmap = ["."] * (self.__xdim * self.__ydim)
-        # This seems to avoid bad copies
-        # build map
         y = 0
         for line in split:
             x = 0
             for elem in line:
-                self.__elements[(x, y)] = elem
+                if self.__isAsteroid(elem):
+                    self.__asteroids[(x, y)] = 0
                 x += 1
             y += 1
-        # go through again, process map
-        y = 0
-        for line in split:
-            x = 0
-            for elem in line:
-                if not self.isAsteroid((x, y)):
-                    x += 1
-                    continue
-                count = self.__radarSweep((x, y))
-                if count != 0:
-                    temp = x + (y * self.__ydim)  # TODO delete me
-                    self.strmap[x + (y * self.__ydim)] = str(count)
-                if not count in self.__processed:
-                    self.__processed[count] = [(x, y)]
-                else:
-                    self.__processed[count].append((x, y))
-                x += 1
-            y += 1
+        for asteroid in self.__asteroids:
+            self.__asteroids[asteroid] = self.__radarSweep(asteroid)
+        pass
 
-    def __radarSweep(self, coordinate: Coordinate) -> int:
-        xcor, ycor = coordinate
-        visited = set()
-        asteroid_count = 0
-        for y in range(-self.__ydim + 1, self.__ydim):
-            if y + ycor >= self.__ydim or y + ycor < 0:
-                continue  # out of range
-            for x in range(-self.__xdim + 1, self.__xdim):
-                if x + xcor >= self.__xdim or x + xcor < 0:
-                    continue  # also out of range
-                ray = (0, 0)
-                if x == 0:
-                    # we need to avoid 0 division
-                    if y == 0:
-                        ray = (0, 0)
-                    elif y < 0:
-                        ray = (0, -1)
-                    elif y > 0:
-                        ray = (0, 1)
-                else:
-                    denom = gcd(y, x)
-                    ray = (y // denom, x // denom)
-                if ray in visited:
-                    continue  # we've processed this slope, do nothing
-                visited.add(ray)
-                asteroid_count += self.__processSlope(coordinate, ray)
-        return asteroid_count
+    def __radarSweep(self, asteroid: Coordinate) -> int:
+        seen = set()
+        count = 0
+        for a in self.__asteroids:
+            if a == asteroid:  # This is ourselves
+                # seen.add(None)
+                continue
+            angle = atan2(a[1] - asteroid[1], a[0] - asteroid[0])
+            if not angle in seen:
+                seen.add(angle)
+        return len(seen)
 
-    def __processSlope(self, coordinate: Coordinate, ray: Coordinate) -> int:
-        if not self.isAsteroid(coordinate):
-            return 0
-        # Count self only if ray is (0, 0)
-        if ray == (0, 0):
-            return 1
-        plusx, plusy = ray
-        x, y = coordinate  # starting here, follow ray
-        asteroid_count = 0
-        x += plusx
-        y += plusy
-        while x >= 0 and y >= 0 and x < self.__xdim and y < self.__ydim:
-            if self.isAsteroid((x, y)):
-                asteroid_count += 1
-                break
-            x += plusx
-            y += plusy
-        return asteroid_count
-
-    def isAsteroid(self, coordinate: Coordinate) -> bool:
-        return self.__elements[coordinate] == "#"
+    @staticmethod
+    def __isAsteroid(char: str) -> bool:
+        return char == "#"  # `#` is defined as the asteroid character in problem
 
     def monitorLocation(self) -> Tuple[Coordinate, int]:
-        maxCount = max(self.__processed.keys())
-        ret = (self.__processed[maxCount][0], maxCount)
-        return ret
+        out = 0
+        for asteroid in self.__asteroids:
+            pass
+            # TODO reverse dict? Maybe?
+        return out
 
     def __str__(self):
-        strmap = "".join(self.strmap)
-        out = ""
-        count = 0
-        for s in strmap:
-            out += s
-            count += 1
-            if count % self.__ydim == 0:
-                out += "\n"
-        return out
+        return ""
 
 
 class MapTester(unittest.TestCase):
@@ -120,7 +66,6 @@ class MapTester(unittest.TestCase):
 ....#
 ...##"""
         amap = AsteroidMap(mapdata)
-        temp = str(amap)
         output, count = amap.monitorLocation()
         self.assertEqual(output, (3, 4))
         self.assertEqual(count, 8)
